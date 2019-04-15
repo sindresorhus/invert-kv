5b8f23c2bdc810339e6b1be3be914c234c514e24
Rodrigo Cesar <digocesar@yahoo.com.br>
2019-04-15 10:26:02 -0300

    Fix build test
    Fix build test suggestions

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict';
-module.exports = function(object) {
+module.exports = function (object) {
 	if (typeof object !== 'object') {
 		throw new TypeError('Expected an object');
 	}
 
 	const ret = {};
 
-	for (var key in Object.keys(object)) {
-		const value = object[key];
-		ret[value] = key;
-	}
+	Object.keys(object)
+		.forEach(function (key) {	// eslint-disable-line prefer-arrow-callback
+			const value = object[key];
+			ret[value] = key;
+		});
 
 	return ret;
 };
